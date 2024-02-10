6edd8fd56b6e694429df03c62cc1d61bb81a01a2
tnifey <tnifey@gmail.com>
2024-02-10 20:12:25 +0100

    alias identity-card -> dowod

--- src/modules/identity_card/command.rs
+++ src/modules/identity_card/command.rs
@@ -2,7 +2,7 @@ use clap::*;
 use crate::types::Output;
 
 #[derive(Debug, Clone, Parser)]
-#[command(about = "Generate a random polish identity card number")]
+#[command(about = "Generate a random polish identity card number", alias="dowod")]
 pub struct Arguments {}
 
 pub fn handle(_args: Arguments) -> Output {
